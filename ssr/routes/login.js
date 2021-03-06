1a4a12c6a4db38f3d2a26b5fb4adcea478a1f876
Rhatalin <danielflo1997@hotmail.com>
2021-03-06 21:24:34 +0100

    Improve login by case insensitive username check

--- ssr/routes/login.js
+++ ssr/routes/login.js
@@ -17,18 +17,18 @@ router.post('/', async function (req, res) {
       { title: 'Login failed', errors: [{ error: { text: 'Please put in your username and password.' }, },] }
     );
   }
-  const username = req.body.username.trim();
+  const username = req.body.username.trim().toLowerCase();
   const password = req.body.password;
 
-  // Check if username exists
-  let user = await userM.findOne({ username }).exec();
+  // Check if username exists (case insensitive)
+  let user = await userM.findOne({ username: { $regex: username, $options: 'i' } }).exec();
   if (!user) {
     return res.render('login',
       { title: 'Login failed', errors: [{ error: { text: `There is no user registered as ${username}.` }, },] }
     );
   }
   // Check credentials
-  user = await userM.findOne({ username, password: hash(password) }).exec();
+  user = await userM.findOne({ username: { $regex: username, $options: 'i' }, password: hash(password) }).exec();
   if (!user) {
     return res.render('login',
       { title: 'Login failed', errors: [{ error: { text: 'The login details you entered are incorrect. Please try again.' }, },] }
